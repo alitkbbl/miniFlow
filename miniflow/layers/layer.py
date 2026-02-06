0217b60e214da381a77ce34d80c5180e167699af
alitkbbl <alitaghi8305@gmail.com>
2026-02-06 10:43:17 +0330

    Phase 1 of the project has been completed.

--- miniflow/layers/layer.py
+++ miniflow/layers/layer.py
@@ -2,6 +2,7 @@ class Layer:
     def __init__(self):
         self.input = None
         self.output = None
+        self.is_built = False # برای بررسی اینکه آیا وزن‌ها مقداردهی شده‌اند
 
     def forward(self, input_data, training=True):
         raise NotImplementedError
